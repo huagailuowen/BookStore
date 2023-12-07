80377d985a156e266b7c9a544bee4ff26f3c8514
huagailuowen <2146964802@qq.com>
2023-12-07 19:27:44 +0800

    save

---
+++ build/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/周宸源/Desktop/大学/学习/程序/Bookstore-2023/src/accounts.cpp" "CMakeFiles/code.dir/src/accounts.cpp.o" "gcc" "CMakeFiles/code.dir/src/accounts.cpp.o.d"
+  "/mnt/c/Users/周宸源/Desktop/大学/学习/程序/Bookstore-2023/src/main.cpp" "CMakeFiles/code.dir/src/main.cpp.o" "gcc" "CMakeFiles/code.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/code.dir/src/accounts.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/code.dir/src/accounts.cpp.o: \
+ /mnt/c/Users/周宸源/Desktop/大学/学习/程序/Bookstore-2023/src/accounts.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/c/Users/周宸源/Desktop/大学/学习/程序/Bookstore-2023/src/accounts.h
